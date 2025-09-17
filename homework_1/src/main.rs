9b285ec5f65da9195678ff8b140e2d0643a00545
AmandoRamirez33 <amram217312@gmail.com>
2025-09-17 15:25:44 +0000

    My first program

---
+++ homework_1/src/main.rs
@@ -0,0 +1,16 @@
+fn assignment1(){
+    const freezePointinF:i32 = 32;
+    fn fahrenheit_to_celsius(fahrenheit) ->{
+        Celsius = (5/9)* (fahrenheit - freezePointinF);
+        return Celsius;
+    };
+    fn celsius_to_fahrenheit(Celsius){
+
+
+    }
+
+}
+
+fn main(){
+    assignment1();
+}
